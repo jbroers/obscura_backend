53ce1f550a431a973d064aa22e887490ba2b33bf
Jorn Broers <89794529+jbroers@users.noreply.github.com>
2025-09-09 14:29:25 +0200

    Initial commit

---
+++ src/main/java/com/example/obscura_backend/ObscuraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.obscura_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObscuraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObscuraBackendApplication.class, args);
+    }
+
+}
